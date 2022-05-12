31d1b2cf495367d29ff1a69f1c6a1ebede818944
ahddhhhhcfth <105232026+ahddhhhhcfth@users.noreply.github.com>
2022-05-13 00:44:55 +0400

    005 Javada sert operatotu

---
+++ src/ders/javaCondition.java
@@ -0,0 +1,28 @@
+package ders;
+
+public class javaCondition {
+
+	public static void main(String[] args) {
+		int age =18;
+		boolean enter=false;
+		if(age>=18) {
+			enter = true;
+		}else {
+			enter=false;
+		}
+		if(enter) {
+			System.out.println("sene giris olmaz");
+		} else {
+			System.out.println("sene giris olar");
+		}
+		
+			
+			}
+			
+		
+		
+
+
+	}
+
+
