03d9a8762022f4f9e7b6dc510ab50cb41f50070c
ahddhhhhcfth <105232026+ahddhhhhcfth@users.noreply.github.com>
2022-05-12 22:29:27 +0400

    Update mainclass.java

--- src/ders/mainclass.java
+++ src/ders/mainclass.java
@@ -2,7 +2,7 @@ package ders;
 
 public class mainclass {
 public static void main(String[] args){
-	System.out.println("Anar");
-	
+	System.out.println("Anar")	;
+	System.out.println("Abbasov");	
 }
 }
