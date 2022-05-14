47e05bfeb66a60b86452f283d5b2329caa56f7b7
ahddhhhhcfth <105232026+ahddhhhhcfth@users.noreply.github.com>
2022-05-14 13:53:53 +0400

    003 javada primitiv tipler

---
+++ src/ders/deyiwen.java
@@ -0,0 +1,33 @@
+package ders;
+
+public class deyiwen {
+
+	public static void main(String[] args) {
+int b=20;
+System.out.println("b = "+b);
+short s=57;
+System.out.println("s = "+s);
+byte d=3;
+System.out.println("d = "+d);
+long l=500;
+System.out.println("l = "+l);
+
+
+float f=3.5f;
+System.out.println("f = "+f);
+
+double g=9;
+System.out.println("g = "+g);
+
+boolean has=true;
+System.out.println("has ="+has);
+boolean home=false;
+System.out.println("home = "+home);
+
+char c='q';
+System.out.println("c = "+c);
+
+
+	}
+
+}
---
+++ src/string.java
@@ -0,0 +1,11 @@
+
+public class string {
+
+	public static void main(String[] args) {
+		 for(int i=7;i<40; i++);
+		 System.out.println(i)
+		 if(i%7==0);
+
+	}
+
+}
