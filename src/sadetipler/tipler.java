76e5b0b62447cde800fb3428637e4193c01687cf
ahddhhhhcfth <105232026+ahddhhhhcfth@users.noreply.github.com>
2022-05-15 22:15:55 +0400

    tipler

---
+++ src/sadetipler/tipler.java
@@ -0,0 +1,17 @@
+package sadetipler;
+
+public class tipler {
+
+	public static void main(String[] args) {
+		int ededim=13;
+		boolean sadedir=true;
+		for (int i = 2; i <= ededim-1; i++) {
+			if(ededim%i==0);{
+				sadedir=false;
+			}
+			
+		}
+
+	}
+
+}
