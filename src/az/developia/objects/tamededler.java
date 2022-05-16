2f0b721c2e42dbd131b31f816fcd4d89581df443
ahddhhhhcfth <105232026+ahddhhhhcfth@users.noreply.github.com>
2022-05-16 15:27:08 +0400

    tam ededler

---
+++ src/az/developia/objects/tamededler.java
@@ -0,0 +1,20 @@
+package az.developia.objects;
+
+public class tamededler {
+
+	public static void main(String[] args) {
+		int ededim=14;
+		boolean sadedir=true;
+		for (int i = 2; i <=ededim-1; i++) {
+			if(ededim%i==0);{
+				sadedir=false;
+			
+		}
+			
+		}
+		System.out.println(sadedir);
+		}
+
+	}
+
+
