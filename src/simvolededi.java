9730212ca13a43d995c2139886ebc730f082d05d
ahddhhhhcfth <105232026+ahddhhhhcfth@users.noreply.github.com>
2022-05-16 15:25:02 +0400

    003simvol tipi

--- src/simvolededi.java
+++ src/simvolededi.java
@@ -2,10 +2,9 @@
 public class simvolededi {
 
 	public static void main(String[] args) {
-for (int i = 97; i < 122; i++) {
-	System.out.println((char)i);
+System.out.println((int)'a');
 	
 }
 	}
 
-}
+
