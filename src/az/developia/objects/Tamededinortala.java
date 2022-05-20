ebf94016e638add46857d084454c867e4ad45df5
ahddhhhhcfth <105232026+ahddhhhhcfth@users.noreply.github.com>
2022-05-20 11:44:29 +0400

    tamin ortalamsi

---
+++ src/az/developia/objects/Tamededinortala.java
@@ -0,0 +1,14 @@
+package az.developia.objects;
+
+public class Tamededinortala {
+
+	public static void main(String[] args) {
+	int age=1;
+	for (int i = 2; i <= 10; i++) {
+		if(age%i==5);
+		boolean dogrudur=true;
+	}
+
+	}
+
+}
