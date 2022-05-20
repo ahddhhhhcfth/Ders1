dd21aefb8ddb8f35bcd3a9cdf60736dc8debaebc
ahddhhhhcfth <105232026+ahddhhhhcfth@users.noreply.github.com>
2022-05-20 11:30:29 +0400

    doublefor

---
+++ src/az/developia/objects/doublefor.java
@@ -0,0 +1,14 @@
+package az.developia.objects;
+
+public class doublefor {
+
+	public static void main(String[] args) {
+		double age=0.1;
+		double b=1.5;
+		for (double i = 0.2;i<= 10; i++) {
+			b++;
+		}
+
+	}
+
+}
