8d28941705be59174ebf72064112794f82771715
ahddhhhhcfth <105232026+ahddhhhhcfth@users.noreply.github.com>
2022-05-20 11:36:38 +0400

    while tam eded

---
+++ src/az/developia/objects/whileTAmEdedler.java
@@ -0,0 +1,15 @@
+package az.developia.objects;
+
+public class whileTAmEdedler {
+
+	public static void main(String[] args) {
+		int age=0;
+		int s=1;
+		while(age<=10) {
+			s++;
+			
+		}
+
+	}
+
+}
