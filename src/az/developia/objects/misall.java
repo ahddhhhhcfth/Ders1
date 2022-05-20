3d817c4db1920714b1db6358b11c371286841ca5
ahddhhhhcfth <105232026+ahddhhhhcfth@users.noreply.github.com>
2022-05-20 11:55:58 +0400

    misal

---
+++ src/az/developia/objects/misall.java
@@ -0,0 +1,15 @@
+package az.developia.objects;
+
+public class misall {
+
+	public static void main(String[] args) {
+		int age=20;
+		double s=1.1;
+		for (double i = 1.5; i <= 30; i++) {
+			s++;
+			
+		}
+
+	}
+
+}
