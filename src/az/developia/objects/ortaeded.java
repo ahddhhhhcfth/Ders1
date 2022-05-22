d7f75b2f6a25da9a750a842f1439db18079f94a9
ahddhhhhcfth <105232026+ahddhhhhcfth@users.noreply.github.com>
2022-05-22 11:47:28 +0400

    ortaeded0055

--- src/az/developia/objects/ortaeded.java
+++ src/az/developia/objects/ortaeded.java
@@ -3,13 +3,14 @@ package az.developia.objects;
 public class ortaeded {
 
 	public static void main(String[] args) {
-		int eded=345;
+		int eded=654;
 		int say=0;
 		int cem=0;
 		while(eded>0) {
 			int qaliq=eded%10;
 			cem+=qaliq;
 			eded/=10;
+			say++;
 		}
 System.out.println("cem "+ cem);
 System.out.println("averge "+ cem/say);
