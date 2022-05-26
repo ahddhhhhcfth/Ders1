42028ac373bd286fb583a2632117f990be5068fd
ahddhhhhcfth <105232026+ahddhhhhcfth@users.noreply.github.com>
2022-05-26 22:42:36 +0400

    06ders

---
+++ src/ders6.java
@@ -0,0 +1,23 @@
+
+public class ders6 {
+String name;
+String surname;
+int age;
+int maaw;
+
+void printMyAllInfo() {
+	System.out.println("name :" +name);
+	System.out.println("surname :"+surname);
+	System.out.println("age :"+age);
+	System.out.println("maaw :"+maaw);
+}
+void hesablaMaw() {
+	maaw=age*10;
+}
+
+{
+		
+
+	}
+
+}
---
+++ src/ders7.java
@@ -0,0 +1,22 @@
+
+public class ders7 {
+
+	public static void main(String[] args) {
+		String name="Anar";
+		String surname="Abbasov";
+		int age=23;
+		int maaw=70;
+
+		ders6 anar=new ders6();
+		anar.name="Anar";
+		anar.surname="Abbasov";
+		anar.age=23;
+		anar.maaw=70;
+		
+		System.out.println(anar.name);
+		System.out.println(anar.surname);
+		System.out.println(anar.age);
+		System.out.println(anar.maaw);
+	}
+
+}
