88546b2ff25e900620b9910c872e5902fc764adf
ahddhhhhcfth <105232026+ahddhhhhcfth@users.noreply.github.com>
2022-05-30 15:22:24 +0400

    ortamasiv 1ci variant

---
+++ src/az/developia/objects/ortasintapanmaasiv.java
@@ -0,0 +1,30 @@
+package az.developia.objects;
+
+public class ortasintapanmaasiv {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=6;
+		int[] nomre=new int[8];
+		nomre[0]=5;
+		nomre[1]=6;
+		nomre[2]=7;
+		nomre[3]=4;
+		nomre[4]=3;
+		nomre[5]=2;
+		nomre[6]=1;
+		nomre[7]=8;
+		int bolun=2;
+		boolean zamenoldu=true;
+		int saygac=1;
+		while(zamenoldu) {
+			zamenoldu=false;
+			for (int i = 1; i < 8/2; i++) {
+				System.out.println(i);
+				 
+					
+				}
+	}
+
+		}
+	}
