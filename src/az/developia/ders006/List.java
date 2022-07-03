eaeb6319b82928bf2ad9b11cc219ad0565967080
ahddhhhhcfth <105232026+ahddhhhhcfth@users.noreply.github.com>
2022-07-03 22:21:39 +0400

    ders17

--- src/az/developia/ders006/List.java
+++ src/az/developia/ders006/List.java
@@ -1,26 +1,40 @@
 package az.developia.ders006;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.ListIterator;
 
 public class List {
 
 	public static void main(String[] args) {
-		ArrayList <String> name =ArrayList <String> ( );
+		ArrayList <String> names = new ArrayList <String> ( );
 		names.add("Anar");
 		names.add("Misa");
 		names.add("Sahin");
 		names.add("Ferid");
 		names.add("Pervin");
 System.out.println(names);
+  Collection.sort(names);
+ System.out.println(names);
 
-ListIterator<String> i= names.Listiterator();
-
-while(i.hasNext()) {
-	if(i.next().equals("Pervin")) {
-		i.remove();
+ 
+ ArrayList <Integer> n = new ArrayList<> ( );
+	n.add(4);
+	n.add(7);
+	n.add(8);
+	n.add(6);
+	n.add(5);
+	System.out.println(n);
+	Collection.sort(n);
+	System.out.println(n);
+	
+	
+	
+	
+	
+	
 	}
-}
+
 	}
 
-}
+
