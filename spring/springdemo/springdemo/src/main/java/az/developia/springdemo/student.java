6b0eecd261bee71812f97dd0823f404f36801494
ahddhhhhcfth <105232026+ahddhhhhcfth@users.noreply.github.com>
2022-08-29 20:42:46 +0400

    spring 3

--- spring/springdemo/springdemo/src/main/java/az/developia/springdemo/student.java
+++ spring/springdemo/springdemo/src/main/java/az/developia/springdemo/student.java
@@ -1,6 +1,11 @@
 package az.developia.springdemo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class student {
 	public String name;
-
+     student(){
+	name="Anar";
+}
 }
